b71bbacb61b399d4c79c869f3ad4f7e9edde3854
HWK <sivagin@hotmail.com>
2017-03-29 23:41:18 +0200

    Update of MySQL connector and view's

--- src/by/gsu/course/mysql/MySQLConnector.java
+++ src/by/gsu/course/mysql/MySQLConnector.java
@@ -1,20 +1,45 @@
 package by.gsu.course.mysql;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MySQLConnector {
-	public static Connection getConnection(final String baseName, final String username, final String password) {
+	private Connection dbConnection = null;
+	private final String basename;
+	private final String username;
+	private final String password;
+
+	public MySQLConnector(final String basename, final String username, final String password) {
+		this.basename = basename;
+		this.username = username;
+		this.password = password;
+	}
+
+	public void createConnection() {
 		try {
-			final String url = "jdbc:sqlserver://localhost:1433;database=SSivagin;integratedSecurity=true;";
-
+			final String url = "jdbc:sqlserver://SIARHEI-PC\\SQLEXPRESS;database=" + this.basename;
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			final Connection dbConnection = DriverManager.getConnection(url, username, password);
-
-			return dbConnection;
+			this.dbConnection = DriverManager.getConnection(url, this.username, this.password);
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
-		return null;
+	}
+
+	public Connection getDbConnection() {
+		return this.dbConnection;
+	}
+
+	public void getTable() throws SQLException {
+		final DatabaseMetaData metaData = this.getDbConnection().getMetaData();
+		final ResultSet result = metaData.getTables(null, null, "%", null);
+
+		while (result.next()) {
+			System.out.println(result.getString(3));
+		}
+
+		result.close();
 	}
 }
--- src/by/gsu/course/view/AuthViewController.java
+++ src/by/gsu/course/view/AuthViewController.java
@@ -1,10 +1,15 @@
 package by.gsu.course.view;
 
+import java.sql.SQLException;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
 import by.gsu.course.mysql.MySQLConnector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class AuthViewController {
 	@FXML
@@ -15,6 +20,8 @@ public class AuthViewController {
 	private TextField password;
 	@FXML
 	private Label message;
+	@FXML
+	private ImageView logotype;
 
 	public AuthViewController() {
 	}
@@ -44,10 +51,18 @@ public class AuthViewController {
 	}
 
 	@FXML
-	private void handleButtonAction(final ActionEvent event) {
-		if ((MySQLConnector.getConnection("SSivagin", this.username.getText(), this.password.getText())) != null) {
+	private void handleButtonAction(final ActionEvent event) throws SQLServerException {
+		final MySQLConnector connector = new MySQLConnector("SSivagin", this.username.getText(),
+				this.password.getText());
+		connector.createConnection();
+		if (connector.getDbConnection() != null) {
 			this.message.setText("Success connection");
 			this.message.setVisible(true);
+			try {
+				connector.getTable();
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			}
 		} else {
 			this.message.setText("Connection error");
 			this.message.setVisible(true);
